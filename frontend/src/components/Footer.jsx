6b296722366dd68f6b8200a94798e682b17da943
Binay <binayprogramming@gmail.com>
2020-12-25 23:38:54 +0100

    Header footer and main components started,navbar ready

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Col className='text-center py-3'>Copyright &copy; E-Shop</Col>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
