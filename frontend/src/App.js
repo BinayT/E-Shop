1c0dcfd2f9b75729b02abe51a31496904a209a12
Binay <binayprogramming@gmail.com>
2020-12-25 23:58:28 +0100

    Home Screen product listing done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Container } from 'react-bootstrap';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main className='py-3'>
         <Container>
           <h1>Welcome to E-Shop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
