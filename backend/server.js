d672eac6f5883c5ab2a5a540ba883c10659b1d4a
Binay <binayprogramming@gmail.com>
2020-12-26 14:58:47 +0100

    Serving products from backend routes now

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import products from './data/products.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API RUNNING CORRECTLY');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((product) => product._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(5000, console.log('Server running on port 5000'));
