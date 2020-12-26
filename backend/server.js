19126a10b9670f7263cf70ec5a8e4592233b34a0
Binay <binayprogramming@gmail.com>
2020-12-26 20:24:51 +0100

    Colors package installed and applied for dev env

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
 
 import products from './data/products.js';
 import connectDB from './config/db.js';
@@ -26,4 +27,7 @@ app.get('/api/product/:id', (req, res) => {
 const port = process.env.PORT || 5000;
 const env = process.env.NODE_ENV;
 
-app.listen(5000, console.log(`Server running on port ${port} in ${env} mode.`));
+app.listen(
+  5000,
+  console.log(`Server running on port ${port} in ${env} mode.`.yellow.bold)
+);
