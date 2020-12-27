1e257a7d6d823a1de35a4a66ff51ed092acb05cf
Binay <binayprogramming@gmail.com>
2020-12-27 14:17:47 +0100

    Redux store made

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,15 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import store from './store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
