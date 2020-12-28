ce534ecac4c5d61177bdb091151fd9dcf811d963
Binay <binayprogramming@gmail.com>
2020-12-28 15:12:00 +0100

    Cleaning up the routes applying controllers

---
+++ backend/controllers/productController.js
@@ -0,0 +1,23 @@
+import Product from '../models/productModel.js';
+
+const getProducts = async (_, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (error) {
+    res
+      .status(404)
+      .json({ message: 'Product not found', error: `Error: ${error.message}` });
+  }
+};
+
+export { getProducts, getProductById };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,31 +1,19 @@
 import express from 'express';
-import Product from '../models/productModel.js';
 const router = express.Router();
+
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js';
 
 //@Desc GET all products
 //@route GET api/products
 //@access Public
-router.get('/', async (_, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json(error.message);
-  }
-});
+router.route.('/').get(getProducts);
 
 //@Desc GET a product
 //@route GET api/products/:id
 //@access Public
-router.get('/:id', async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    res.status(200).json(product);
-  } catch (error) {
-    res
-      .status(404)
-      .json({ message: 'Product not found', error: `Error: ${error.message}` });
-  }
-});
+router.route.('/:id').get(getProductById)
 
 export default router;
