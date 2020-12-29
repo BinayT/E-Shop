83b3c1fd0ae04412d83c4b514beb1fffa6cea3c4
Binay <binayprogramming@gmail.com>
2020-12-29 21:18:18 +0100

    Action for updating user made

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,7 +7,11 @@ import {
   productItemReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+} from './reducers/userReducers';
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
@@ -28,6 +32,7 @@ const reducer = combineReducers({
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
 });
 
 const middleware = [thunk];
