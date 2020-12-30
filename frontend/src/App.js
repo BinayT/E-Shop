f601db16f34f6f553c19da5539d914d099b59aa1
Binay <binayprogramming@gmail.com>
2020-12-30 14:43:38 +0100

    Shipping screen started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <main className='py-3'>
         <Container>
           <Route path='/login' component={LoginScreen} />
+          <Route path='/shipping' component={ShippingScreen} />
           <Route path='/profile' component={ProfileScreen} />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/product/:id' component={ProductScreen} />
---
+++ frontend/src/screens/ShippingScreen.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ShippingScreen = () => {
+  return <div>This is a shipping screen</div>;
+};
+
+export default ShippingScreen;
