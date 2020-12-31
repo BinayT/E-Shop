613c1e6e7950f87e4e8905ee4963d264c199baec
Binay <binayprogramming@gmail.com>
2020-12-31 13:43:54 +0100

    Reducers made+imported on store.js for order

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,6 +13,7 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from './reducers/userReducers';
+import { orderCreateReducer } from './reducers/orderReducers';
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
@@ -42,6 +43,7 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  orderCreate: orderCreateReducer,
 });
 
 const middleware = [thunk];
