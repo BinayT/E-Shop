85390903bafa137eb50628d4882340fe7f120062
Binay <binayprogramming@gmail.com>
2021-01-03 00:30:41 +0100

    User Edit Screen for admin is done

---
+++ frontend/src/screens/UserEditScreen.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Col, Row, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Loading from '../components/Loading';
+import ErrorMessage from '../components/ErrorMessage';
+import FormContainer from '../components/FormContainer';
+import { getUserDetails } from '../actions/userActions';
+
+const UserEditScreen = ({ location, history, match }) => {
+  const userId = match.params.id;
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const dispatch = useDispatch();
+  const userDetails = useSelector((state) => state.userDetails);
+  const { loading, error, user } = userDetails;
+
+  useEffect(() => {}, []);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <Link className='btn btn-dark my-3 gobackbutton' to='/admin/userlist'>
+        <i className='far fa-hand-point-left' /> Go back
+      </Link>
+      <FormContainer>
+        <h1>Edit User: {user.name}</h1>
+        {loading ? (
+          <Loading />
+        ) : error ? (
+          <ErrorMessage variant='danger'>{error}</ErrorMessage>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId='name'>
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                maxLength='10'
+                type='text'
+                placeholder='Enter Your Name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId='email'>
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type='email'
+                placeholder='Enter Email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId='isAdmin'>
+              <Form.Check
+                type='checkbox'
+                label='Is Admin'
+                checked={isAdmin}
+                onChange={(e) => setIsAdmin(e.target.checked)}
+              ></Form.Check>
+            </Form.Group>
+            <Button variant='primary' type='submit'>
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  );
+};
+
+export default UserEditScreen;
