8fe43902f5b93a80d4f7fffde6c16851ef1e085f
Binay <binayprogramming@gmail.com>
2021-01-04 00:08:01 +0100

    Multer's storage configed

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import multer from 'multer';
+import path from 'path';
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+export default router;
