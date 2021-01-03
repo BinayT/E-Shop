c8580c53e9eae2f937e67034f74b03bb0034b395
Binay <binayprogramming@gmail.com>
2021-01-03 16:35:15 +0100

    Actions also made for creating a new product

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -69,3 +69,28 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
     });
   }
 };
+
+export const createProduct = () => async (dispatch, getState) => {
+  try {
+    dispatch({ type: CREATE_PRODUCT_REQUEST });
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.post(`/api/products`, {}, config);
+    dispatch({ type: CREATE_PRODUCT_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: CREATE_PRODUCT_ERROR,
+      payload: error.response.data.message
+        ? error.response.data.message
+        : error.message,
+    });
+  }
+};
