c2153bdebdf8ad4e4292e4eabd3c4af7ea56bc41
Binay <binayprogramming@gmail.com>
2021-01-06 13:48:37 +0100

    Modified existing product actions to consider the page number

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -21,10 +21,14 @@ import {
   PRODUCT_CREATE_REVIEW_ERROR,
 } from '../constants/productConstants';
 
-export const listProducts = (input = '') => async (dispatch) => {
+export const listProducts = (input = '', pageNumber = '') => async (
+  dispatch
+) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
-    const { data } = await axios.get(`/api/products?input=${input}`);
+    const { data } = await axios.get(
+      `/api/products?input=${input}&pageNumber=${pageNumber}`
+    );
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
     dispatch({
