c5c13c84cc68a5259e189062aaa80c3de3990a1b
Binay <binayprogramming@gmail.com>
2021-01-06 15:49:22 +0100

    Prepearing for deployment, backend scripts made

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(express.json());
 
-process.env.NODE_ENV === 'DEVELOPMENT' && app.use(morgan('dev'));
+process.env.NODE_ENV === 'development' && app.use(morgan('dev'));
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -29,9 +29,18 @@ app.use('/api/upload', uploadRoutes);
 app.get('/api/config/paypal', (_, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
-
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => res.send('API is running ....'));
+}
 
 app.use(notFound);
 
