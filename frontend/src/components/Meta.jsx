29640009f80f3734a37eeff5e84304457b0925ef
Binay <binayprogramming@gmail.com>
2021-01-06 15:31:22 +0100

    Made reusuable meta tag for any pages with its own title description and keywords

---
+++ frontend/src/components/Meta.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keywords' content={keywords} />
+    </Helmet>
+  );
+};
+
+export default Meta;
